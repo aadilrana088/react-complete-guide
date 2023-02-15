086fadd3c1ee1d93c7396e436c8bd35d2a4f8ae9
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-02-16 00:45:25 +0530

    Added state to ExpensItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Card from './Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 const ExpenseItem = ({ title, amount, date }) => {
-    
+    const [newTitle, setNewTitle] = useState(title);
+    const clickHandler = () => {
+        setNewTitle("Updated Title")
+        console.log(newTitle);
+    }
     return (
         <Card className="expense-item">
             <ExpenseDate date= {date} />
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{newTitle}</h2>
                 <div className="expense-item__price">${amount}</div>
+                <button onClick={clickHandler}>Click</button>
             </div>
         </Card>
     );
