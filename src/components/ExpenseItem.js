c634a31d267c180e93d5936cc32acac966a6d530
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-02-15 23:26:51 +0530

    seprate component for ExpenseItem & ExpenseDate

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,11 @@
 import React from 'react';
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 const ExpenseItem = ({ title, amount, date }) => {
-    const month = date.toLocaleString('en-US', { month: 'long' });
-    const day = date.toLocaleString('en-US', { day: '2-digit' });
-    const year = date.getFullYear();
+    
     return (
         <div className="expense-item">
-            <div>
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{day}</div>
-            </div>
+            <ExpenseDate date= {date} />
             <div className="expense-item__description">
                 <h2>{title}</h2>
                 <div className="expense-item__price">${amount}</div>
