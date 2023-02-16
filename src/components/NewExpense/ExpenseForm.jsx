7f9e099a9778ea36651b8d6561d7d9a2a55b6ae0
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-02-17 00:17:12 +0530

    clear form and log data on submit form

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -43,6 +43,9 @@ const ExpenseForm = () => {
             amount: enteredAmount,
             date: new Date(enteredDate)
         }
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteDate('');
         console.log(data)
     }
     return (
@@ -50,7 +53,7 @@ const ExpenseForm = () => {
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler} />
+                    <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
@@ -58,6 +61,7 @@ const ExpenseForm = () => {
                         type="number"
                         min="0.01"
                         step="0.01"
+                        value={enteredAmount}
                         onChange={amountChangeHandler}
                     />
                 </div>
@@ -67,6 +71,7 @@ const ExpenseForm = () => {
                         type="date"
                         min="2019-01-01"
                         max="2022-12-31"
+                        value={enteredDate}
                         onChange={dateChangeHandler}
                     />
                 </div>
