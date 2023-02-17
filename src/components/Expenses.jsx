347ab832911e3af410506c862a82408277cc7ab3
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-02-17 21:07:42 +0530

    Print Expense list via map func iterating over list

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -3,35 +3,26 @@ import Card from './Card';
 import ExpenseItem from './ExpenseItem';
 import ExpensesFilter from './ExpensesFilter';
 
-const Expenses = ({expenses}) => {
-    const [filteredYear, setFilteredYear] = useState("2020");
+const Expenses = ({ expenses }) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
     const filterChangeHandler = (selectedYear) => {
-        setFilteredYear(selectedYear)
-        console.log(selectedYear)
-    }
+        setFilteredYear(selectedYear);
+        console.log(selectedYear);
+    };
     return (
         <Card className="expenses">
-            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-            <ExpenseItem
-                title={expenses[0].title}
-                amount={expenses[0].amount}
-                date={expenses[0].date}
+            <ExpensesFilter
+                selected={filteredYear}
+                onChangeFilter={filterChangeHandler}
             />
-            <ExpenseItem
-                title={expenses[1].title}
-                amount={expenses[1].amount}
-                date={expenses[1].date}
-            />
-            <ExpenseItem
-                title={expenses[2].title}
-                amount={expenses[2].amount}
-                date={expenses[2].date}
-            />
-            <ExpenseItem
-                title={expenses[3].title}
-                amount={expenses[3].amount}
-                date={expenses[3].date}
-            />
+            {expenses.map((expense) => (
+                <ExpenseItem
+                    title={expense.title}
+                    amount={expense.amount}
+                    date={expense.date}
+                    key={expense.id}
+                />
+            ))}
         </Card>
     );
 };
