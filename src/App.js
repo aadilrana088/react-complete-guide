99ad50e26ca41cb0b9ba1ebeecbd66ac668ac2cb
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-02-19 19:07:19 +0530

    Created UI and show List of user

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import AddUsers from "./components/Users/AddUsers";
-
+import UsersList from "./components/Users/UsersList";
+const INITIAL_USERS = [
+  {
+    name: "Aadil",
+    age: 26
+  },
+  {
+    name: "Shoiab",
+    age: 23
+  }
+]
 function App() {
+  const [users, setUsers] = useState(INITIAL_USERS)
   return (
     <div>
       <AddUsers />
+      <UsersList users={users} />
     </div>
   );
 }
