3b9acd237bed46b0b769ec22d64689ee1054722e
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-02-20 23:36:26 +0530

    Created UI of error modal

--- src/components/Users/UsersList.jsx
+++ src/components/Users/UsersList.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import Card from '../UI/Card';
+import ErrorModal from '../UI/ErrorModal';
 import classes from './UsersList.module.css';
 const UsersList = (props) => {
     return (
-        <Card className={classes.users}>
-            <ul>
-                {props.users.map((user, index) => (
-                    <li key={index}>
-                        {user.name} is {user.age} years old
-                    </li>
-                ))}
-            </ul>
-        </Card>
+        <>
+            <ErrorModal
+                title="An error occured!"
+                message="Something went wrong!"
+            />
+            <Card className={classes.users}>
+                <ul>
+                    {props.users.map((user, index) => (
+                        <li key={index}>
+                            {user.name} is {user.age} years old
+                        </li>
+                    ))}
+                </ul>
+            </Card>
+        </>
     );
 };
 
