1cbdf898ba5e5fedfcc29ee06823f5535ccdbd0d
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-02-21 23:44:23 +0530

    Created Modal in react portal

--- 03-todo_practice/src/components/UI/ErrorModal.jsx
+++ 03-todo_practice/src/components/UI/ErrorModal.jsx
@@ -1,22 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import Card from './Card';
 import Button from './Button';
 import classes from './ErrorModal.module.css';
-
+const BackDrop = (props) => {
+    return <div className={classes.backdrop} onClick={props.onConfirm} />;
+};
+const ModalOverlay = (props) => {
+    return (
+        <Card className={classes.modal}>
+            <header className={classes.header}>
+                <h2>{props.title}</h2>
+            </header>
+            <div className={classes.content}>
+                <p>{props.message}</p>
+            </div>
+            <footer className={classes.actions}>
+                <Button onClick={props.onConfirm}>Okay</Button>
+            </footer>
+        </Card>
+    );
+};
 const ErrorModal = (props) => {
     return (
         <>
-            <div className={classes.backdrop} onClick={props.onConfirm} />
-            <Card className={classes.modal}>
-                <header className={classes.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={classes.content}>
-                    <p>{props.message}</p>
-                </div>
-                <footer className={classes.actions}>
-                    <Button onClick={props.onConfirm}>Okay</Button>
-                </footer>
-            </Card>
+            {ReactDOM.createPortal(
+                <BackDrop onConfirm={props.onConfirm} />,
+                document.getElementById('backdrop-root')
+            )}
+            {ReactDOM.createPortal(
+                <ModalOverlay
+                    onConfirm={props.onConfirm}
+                    title={props.title}
+                    message={props.message}
+                />,
+                document.getElementById('overlay-root')
+            )}
         </>
     );
 };
