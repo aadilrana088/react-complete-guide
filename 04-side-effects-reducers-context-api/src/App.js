7003ccd046dcad0e7111809ecb9a6857c0c1307e
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-02-23 01:55:14 +0530

    React context implemented basic

--- 04-side-effects-reducers-context-api/src/App.js
+++ 04-side-effects-reducers-context-api/src/App.js
@@ -3,14 +3,15 @@ import React, { useEffect, useState } from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './store/auth-context';
 
 function App() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     useEffect(() => {
-      const storedUserLoggedIn = localStorage.getItem('isLoggedin');
-      if (storedUserLoggedIn === '1') {
-        setIsLoggedIn(true)
-      }
+        const storedUserLoggedIn = localStorage.getItem('isLoggedin');
+        if (storedUserLoggedIn === '1') {
+            setIsLoggedIn(true);
+        }
     }, []);
 
     const loginHandler = (email, password) => {
@@ -22,17 +23,21 @@ function App() {
 
     const logoutHandler = () => {
         setIsLoggedIn(false);
-        localStorage.removeItem('isLoggedin')
+        localStorage.removeItem('isLoggedin');
     };
 
     return (
-        <React.Fragment>
-            <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+        <AuthContext.Provider
+            value={{
+                isLoggedIn: isLoggedIn,
+            }}
+        >
+            <MainHeader onLogout={logoutHandler} />
             <main>
                 {!isLoggedIn && <Login onLogin={loginHandler} />}
                 {isLoggedIn && <Home onLogout={logoutHandler} />}
             </main>
-        </React.Fragment>
+        </AuthContext.Provider>
     );
 }
 
