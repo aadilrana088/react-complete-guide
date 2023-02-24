3c2646f3c4c339edeeea76d1df05d611df3d6249
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-02-25 00:42:16 +0530

    Created Cart and Shows it in modal

---
+++ 05-food-order-app/src/components/Cart/Cart.jsx
@@ -0,0 +1,31 @@
+import Modal from '../UI/Modal';
+import classes from './Cart.module.css';
+
+const Cart = (props) => {
+    const cartItem = (
+        <ul className={classes['cart-items']}>
+            {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map(
+                (item) => (
+                    <li>{item.name}</li>
+                )
+            )}
+        </ul>
+    );
+    return (
+        <Modal>
+            <div>
+                {cartItem}
+                <div className={classes.total}>
+                    <span>Total Amount</span>
+                    <span>35.62</span>
+                </div>
+                <div className={classes.actions}>
+                    <button className={classes['button--alt']}>Close</button>
+                    <button className={classes.button}>Order</button>
+                </div>
+            </div>
+        </Modal>
+    );
+};
+
+export default Cart;
