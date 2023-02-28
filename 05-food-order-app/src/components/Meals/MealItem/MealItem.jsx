16969a9624e179c28d66ddbabbd1957b69d7186c
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-02-28 22:45:42 +0530

    Implemet logic to add item to cart, ueContext and forwardRef

--- 05-food-order-app/src/components/Meals/MealItem/MealItem.jsx
+++ 05-food-order-app/src/components/Meals/MealItem/MealItem.jsx
@@ -1,9 +1,19 @@
+import { useContext } from 'react';
+import CartContext from '../../../store/cart-context';
 import classes from './MealItem.module.css';
 import MealItemForm from './MealItemForm';
 
 const MealItem = (props) => {
+    const cartCtx = useContext(CartContext)
     const price = `₹${props.price.toFixed(2)}`;
-
+    const addToCartHandler = (amount) => {
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price
+        })
+    }
     return (
         <li className={classes.meal}>
             <div>
@@ -12,7 +22,7 @@ const MealItem = (props) => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-                <MealItemForm formId={props.id} />
+                <MealItemForm formId={props.id} onAddToCart={addToCartHandler} />
             </div>
         </li>
     );
