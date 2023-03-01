e118a0a7d27397ea9c9b70c7bae87c894f8747a5
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-03-02 00:24:16 +0530

    Render List of title using useCallback

--- 06-a-look-behind-the-scenes/src/App.js
+++ 06-a-look-behind-the-scenes/src/App.js
@@ -1,28 +1,36 @@
 import React, { useCallback, useState } from 'react';
 
 import './App.css';
+import DemoList from './components/Demo/DemoList';
 import DemoOutput from './components/Demo/DemoOutput';
 import Button from './components/UI/Button/Button';
 
 function App() {
-    const [showParagraph, setShowParagraph] = useState(false);
-    const [allowToggle, setAllowToggle] = useState(false);
-    console.log('App Running');
-    const toggleParaHandler = useCallback(() => {
-        if(allowToggle) {
-            setShowParagraph((prevShowPara) => !prevShowPara)
-        }
-    }, [allowToggle]);
-    const allowToggleHandler = () => {
-        setAllowToggle(true)
-    }
+    // const [showParagraph, setShowParagraph] = useState(false);
+    // const [allowToggle, setAllowToggle] = useState(false);
+    // console.log('App Running');
+    // const toggleParaHandler = useCallback(() => {
+    //     if(allowToggle) {
+    //         setShowParagraph((prevShowPara) => !prevShowPara)
+    //     }
+    // }, [allowToggle]);
+    // const allowToggleHandler = () => {
+    //     setAllowToggle(true)
+    // }
+    const [listTitle, setListTitle] = useState('My List');
+    const changeTitleHandler = useCallback(() => {
+        setListTitle('Updated Title');
+    }, []);
+    const listItems = [5, 3, 1, 10, 9];
     return (
         <div className="app">
-            <h1>Hi there!</h1>
+            {/* <h1>Hi there!</h1>
             <DemoOutput show={showParagraph} />
-            {/* {showParagraph && <p>This is new!</p>} */}
+            {showParagraph && <p>This is new!</p>}
             <Button onClick={toggleParaHandler}>Toggle Paragraph</Button>
-            <Button onClick={allowToggleHandler}>Allow Toggle</Button>
+            <Button onClick={allowToggleHandler}>Allow Toggle</Button> */}
+            <DemoList title={listTitle} items={listItems} />
+            <Button onClick={changeTitleHandler}>Change List Title</Button>
         </div>
     );
 }
