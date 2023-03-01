acb7a81f88390e87df7ccc8515ced3d881b58ed4
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-03-01 23:30:48 +0530

    All child component rerender when state not change

--- 06-a-look-behind-the-scenes/src/App.js
+++ 06-a-look-behind-the-scenes/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
 import Button from './components/UI/Button/Button';
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
     return (
         <div className="app">
             <h1>Hi there!</h1>
-            {showParagraph && <p>This is new!</p>}
+            <DemoOutput show={false} />
+            {/* {showParagraph && <p>This is new!</p>} */}
             <Button onClick={() =>setShowParagraph(!showParagraph)}>
                 Toggle Paragraph
             </Button>
