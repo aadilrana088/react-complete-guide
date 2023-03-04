324173f7e95076f488e8e15a1096f29cb9f9ef87
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-03-04 23:18:04 +0530

    Post request using firebase and show fecthed data

--- 07-http-request/src/App.js
+++ 07-http-request/src/App.js
@@ -15,15 +15,25 @@ function App() {
                 throw new Error('Something went wrong!');
             }
             const data = await response.json();
-            const transformedMovies = data.results.map((movieData) => {
-                return {
-                    id: movieData.episode_id,
-                    title: movieData.title,
-                    openingText: movieData.opening_crawl,
-                    releaseDate: movieData.release_date,
-                };
-            });
-            setMovies(transformedMovies);
+            const loadedMovies = [];
+            for (const key in data) {
+                loadedMovies.push({
+                    id: key,
+                    title: data[key].title,
+                    openingText: data[key].openingText,
+                    releaseDate: data[key].releaseDate
+                })
+            }
+            // const transformedMovies = data.results.map((movieData) => {
+            //     return {
+            //         id: movieData.episode_id,
+            //         title: movieData.title,
+            //         openingText: movieData.opening_crawl,
+            //         releaseDate: movieData.release_date,
+            //     };
+            // });
+            // setMovies(transformedMovies);
+            setMovies(loadedMovies);
         } catch (error) {
             setError(error.message);
         }
@@ -34,8 +44,16 @@ function App() {
         fetchMoviesHandler();
     }, [fetchMoviesHandler]);
 
-    function addMovieHandler(movie) {
-        console.log(movie);
+    async function addMovieHandler(movie) {
+        const response = await fetch('https://react-http-60ae9-default-rtdb.firebaseio.com/movies.json',{
+            method: 'POST',
+            body: JSON.stringify(movie),
+            headers: {
+                'Content-Type': "application/json"
+            }
+        })
+        const data = await response.json();
+        console.log(data);
     }
 
     let content = <p>There are no movies.</p>;
