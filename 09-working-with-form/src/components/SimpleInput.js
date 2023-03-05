67c1a1bde807421cab4b789e8cbd1018426fd461
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-03-05 18:12:41 +0530

    add validation and remove  err msg on submit

--- 09-working-with-form/src/components/SimpleInput.js
+++ 09-working-with-form/src/components/SimpleInput.js
@@ -1,29 +1,37 @@
-import React, {useState, useRef} from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 const SimpleInput = (props) => {
   const [name, setName] = useState('');
-  const [nameIsvalid, SetNameIsvalid] = useState(true)
+  const [nameIsvalid, SetNameIsvalid] = useState(false)
   const inputRef = useRef();
+
+  useEffect(() => {
+    if(nameIsvalid) {
+      console.log("Name Input is not valid")
+    }
+  },[nameIsvalid])
+
   const nameHandler = (e) => {
     setName(e.target.value)
   }
   const handleSubmit = (e) => {
     e.preventDefault();
     if(name.trim() === '') {
-      SetNameIsvalid(false);
+      SetNameIsvalid(true);
       return
     }
     const enteredName = inputRef.current.value
     setName('')
     console.log(name)
     console.log(enteredName)
+    SetNameIsvalid(false)
   }
-  const nameInputClass = !nameIsvalid ? 'form-control invalid' : "form-control";
+  const nameInputClass = nameIsvalid ? 'form-control invalid' : "form-control";
   return (
     <form onSubmit={handleSubmit}>
       <div className={nameInputClass}>
         <label htmlFor='name'>Your Name</label>
         <input type='text' id='name' value={name} ref={inputRef} onChange={nameHandler} />
-        {!nameIsvalid && <p className='error-text'>Name is invalid</p>}
+        {nameIsvalid && <p className='error-text'>Name is invalid</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
