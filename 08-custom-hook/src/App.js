717eebf72b05e0edce0110093ced5fe10fe48097
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-03-05 13:30:20 +0530

    Use usecallback and useeffect in usehttp hook

--- 08-custom-hook/src/App.js
+++ 08-custom-hook/src/App.js
@@ -6,29 +6,25 @@ import useHttp from './hooks/use-http';
 
 function App() {
     const [tasks, setTasks] = useState([]);
-    const transformTasks = (tasksObj) => {
-        const loadedTasks = [];
-
-        for (const taskKey in tasksObj) {
-            loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
-        }
-
-        setTasks(loadedTasks);
-    };
-    const {
-        isLoading,
-        error,
-        sendRequest: fetchTasks,
-    } = useHttp(
-        {
-            url: 'https://react-tasklist-14024-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json',
-        },
-        transformTasks
-    );
+    const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
     useEffect(() => {
-        fetchTasks();
-    }, []);
+        const transformTasks = (tasksObj) => {
+            const loadedTasks = [];
+
+            for (const taskKey in tasksObj) {
+                loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+            }
+
+            setTasks(loadedTasks);
+        };
+        fetchTasks(
+            {
+                url: 'https://react-tasklist-14024-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json',
+            },
+            transformTasks
+        );
+    }, [fetchTasks]);
 
     const taskAddHandler = (task) => {
         setTasks((prevTasks) => prevTasks.concat(task));
