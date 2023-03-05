acd2a28602392f9d2da9461a2b5f83ba0654f78c
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-03-05 18:01:05 +0530

    added validation but not removing eror on submit

--- 09-working-with-form/src/components/SimpleInput.js
+++ 09-working-with-form/src/components/SimpleInput.js
@@ -1,21 +1,29 @@
 import React, {useState, useRef} from 'react'
 const SimpleInput = (props) => {
   const [name, setName] = useState('');
+  const [nameIsvalid, SetNameIsvalid] = useState(true)
   const inputRef = useRef();
   const nameHandler = (e) => {
     setName(e.target.value)
   }
   const handleSubmit = (e) => {
     e.preventDefault();
+    if(name.trim() === '') {
+      SetNameIsvalid(false);
+      return
+    }
     const enteredName = inputRef.current.value
+    setName('')
     console.log(name)
     console.log(enteredName)
   }
+  const nameInputClass = !nameIsvalid ? 'form-control invalid' : "form-control";
   return (
     <form onSubmit={handleSubmit}>
-      <div className='form-control'>
+      <div className={nameInputClass}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' ref={inputRef} onChange={nameHandler} />
+        <input type='text' id='name' value={name} ref={inputRef} onChange={nameHandler} />
+        {!nameIsvalid && <p className='error-text'>Name is invalid</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
