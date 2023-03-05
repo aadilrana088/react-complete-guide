878283f4140452141583e3c4362195ce725c062a
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-03-05 12:21:48 +0530

    Created custom hook and for http and return all state

--- 08-custom-hook/src/App.js
+++ 08-custom-hook/src/App.js
@@ -2,58 +2,49 @@ import React, { useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [tasks, setTasks] = useState([]);
+    const [tasks, setTasks] = useState([]);
+    const transformTasks = (tasksObj) => {
+        const loadedTasks = [];
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-tasklist-14024-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json'
-      );
+        for (const taskKey in tasksObj) {
+            loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+        }
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
+        setTasks(loadedTasks);
+    };
+    const {
+        isLoading,
+        error,
+        sendRequest: fetchTasks,
+    } = useHttp(
+        {
+            url: 'https://react-tasklist-14024-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json',
+        },
+        transformTasks
+    );
 
-      const data = await response.json();
+    useEffect(() => {
+        fetchTasks();
+    }, []);
 
-      const loadedTasks = [];
+    const taskAddHandler = (task) => {
+        setTasks((prevTasks) => prevTasks.concat(task));
+    };
 
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    fetchTasks();
-  }, []);
-
-  const taskAddHandler = (task) => {
-    setTasks((prevTasks) => prevTasks.concat(task));
-  };
-
-  return (
-    <React.Fragment>
-      <NewTask onAddTask={taskAddHandler} />
-      <Tasks
-        items={tasks}
-        loading={isLoading}
-        error={error}
-        onFetch={fetchTasks}
-      />
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <NewTask onAddTask={taskAddHandler} />
+            <Tasks
+                items={tasks}
+                loading={isLoading}
+                error={error}
+                onFetch={fetchTasks}
+            />
+        </React.Fragment>
+    );
 }
 
 export default App;
--- 08-custom-hook/src/components/Tasks/Tasks.js
+++ 08-custom-hook/src/components/Tasks/Tasks.js
@@ -18,7 +18,7 @@ const Tasks = (props) => {
   let content = taskList;
 
   if (props.error) {
-    content = <button onClick={props.onFetch}>Try again</button>;
+    content = <button onClick={props.onFetch}>Try again {props.error}</button>;
   }
 
   if (props.loading) {
