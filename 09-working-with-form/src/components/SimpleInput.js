d652260efe0b665c8e93ebc3abb6cf6a36a46aac
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-03-05 19:14:27 +0530

    refractor and make code small

--- 09-working-with-form/src/components/SimpleInput.js
+++ 09-working-with-form/src/components/SimpleInput.js
@@ -1,43 +1,25 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState } from 'react';
 const SimpleInput = (props) => {
     const [name, setName] = useState('');
-    const [nameIsvalid, SetNameIsvalid] = useState(false);
     const [nameTouched, setNameTouched] = useState(false);
-    const inputRef = useRef();
-
-    useEffect(() => {
-        if (nameIsvalid) {
-            console.log('Name Input is not valid');
-        }
-    }, [nameIsvalid]);
-
+    const nameIsvalid = name.trim() !== ''
+    const nameInputIsValid = !nameIsvalid && nameTouched;
     const nameChangeHandler = (e) => {
         setName(e.target.value);
-        if(e.target.value.trim !== 0) {
-          SetNameIsvalid(true)
-        }
     };
     const nameBlurHandler = (e) => {
         setNameTouched(true);
-        if (name.trim() === '') {
-            SetNameIsvalid(false);
-            return;
-        }
     };
     const handleSubmit = (e) => {
         e.preventDefault();
-        setNameTouched(true);
-        if (name.trim() === '') {
-            SetNameIsvalid(false);
-            return;
+        if(!name) {
+            setNameTouched(true);
+            return
         }
-        const enteredName = inputRef.current.value;
         setName('');
+        setNameTouched(false);
         console.log(name);
-        console.log(enteredName);
-        SetNameIsvalid(true);
     };
-    const nameInputIsValid = !nameIsvalid && nameTouched;
     const nameInputClass = nameInputIsValid
         ? 'form-control invalid'
         : 'form-control';
@@ -49,7 +31,6 @@ const SimpleInput = (props) => {
                     type="text"
                     id="name"
                     value={name}
-                    ref={inputRef}
                     onChange={nameChangeHandler}
                     onBlur={nameBlurHandler}
                 />
