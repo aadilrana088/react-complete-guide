8e83c996cf2ae1e985065251d0b57662295e96ca
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-03-05 13:43:43 +0530

    implement custom hook in New created task

--- 08-custom-hook/src/components/NewTask/NewTask.js
+++ 08-custom-hook/src/components/NewTask/NewTask.js
@@ -1,49 +1,36 @@
-import { useState } from 'react';
+import useHttp from '../../hooks/use-http';
 
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+    const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
+    const createTask = (taskText, taskData) => {
+        const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+        const createdTask = { id: generatedId, text: taskText };
 
-  const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-tasklist-14024-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
+        props.onAddTask(createdTask);
+    };
+    const enterTaskHandler = async (taskText) => {
+        sendTaskRequest(
+            {
+                url: 'https://react-tasklist-14024-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json',
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: { text: taskText },
+            },
+            createTask.bind(null, taskText)
+        );
+    };
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
-
-  return (
-    <Section>
-      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
-      {error && <p>{error}</p>}
-    </Section>
-  );
+    return (
+        <Section>
+            <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+            {error && <p>{error}</p>}
+        </Section>
+    );
 };
 
 export default NewTask;
