332276fc77dcc8da650e33eafc737033e23d64fa
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-03-07 17:48:27 +0530

    Created json db for meals fetch it and displays it

--- 05-food-order-app/src/components/Meals/AvailableMeals.jsx
+++ 05-food-order-app/src/components/Meals/AvailableMeals.jsx
@@ -1,36 +1,58 @@
+import { useEffect, useState } from 'react';
 import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css';
 import MealItem from './MealItem/MealItem';
 
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
+// const DUMMY_MEALS = [
+//     {
+//         id: 'm1',
+//         name: 'Sushi',
+//         description: 'Finest fish and veggies',
+//         price: 22.99,
+//     },
+//     {
+//         id: 'm2',
+//         name: 'Schnitzel',
+//         description: 'A german specialty!',
+//         price: 16.5,
+//     },
+//     {
+//         id: 'm3',
+//         name: 'Barbecue Burger',
+//         description: 'American, raw, meaty',
+//         price: 12.99,
+//     },
+//     {
+//         id: 'm4',
+//         name: 'Green Bowl',
+//         description: 'Healthy...and green...',
+//         price: 18.99,
+//     },
+// ];
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map((meal) => (
+    const [meals, setMeals] = useState([]);
+    useEffect(() => {
+        const fetchMeals = async () => {
+            const response = await fetch(
+                'https://react-meals-ffc27-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json'
+            );
+            const responseData = await response.json();
+            const loadedeMeals = [];
+            for (const key in responseData) {
+                loadedeMeals.push({
+                    id: key,
+                    name: responseData[key].name,
+                    description: responseData[key].name,
+                    price: responseData[key].price,
+                });
+            }
+            setMeals(loadedeMeals);
+        };
+        fetchMeals();
+    }, []);
+
+    const mealsList = meals && meals.map((meal) => (
         <MealItem
             key={meal.id}
             name={meal.name}
