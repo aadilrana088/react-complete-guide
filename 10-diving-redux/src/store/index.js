a73bb643113da08949be6fb34cc0bc51989c0a66
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-03-08 18:19:38 +0530

    created redux store and wrap our app with provider

---
+++ 10-diving-redux/src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from 'redux';
+
+function counterReducer(state = { counter: 0 }, action) {
+    switch (action.type) {
+        case 'counter/incremented':
+            return { counter: state.counter + 1 };
+        case 'counter/decremented':
+            return { counter: state.counter - 1 };
+        default:
+            return state;
+    }
+}
+
+let store = createStore(counterReducer);
+
+export default store;
