fc8115e6813dba713fa55cfd2ab69ba18a17e440
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-03-11 21:37:29 +0530

    Dipatch action from cart item and displays them

--- 11-advanced-redux/src/components/Cart/Cart.js
+++ 11-advanced-redux/src/components/Cart/Cart.js
@@ -4,19 +4,19 @@ import classes from './Cart.module.css';
 import CartItem from './CartItem';
 const Cart = (props) => {
     const cartItem = useSelector((state) => state.cart.items);
-    console.log(cartItem);
     return (
         <Card className={classes.cart}>
             <h2>Your Shopping Cart</h2>
             <ul>
-                {cartItem.map((cart) => (
+                {cartItem.map((item) => (
                     <CartItem
-                        key={cart.id}
+                        key={item.id}
                         item={{
-                            title: cart.name,
-                            quantity: cart.quantity,
-                            total: cart.totalPrice,
-                            price: cart.price,
+                            id: item.id,
+                            title: item.name,
+                            quantity: item.quantity,
+                            total: item.totalPrice,
+                            price: item.price,
                         }}
                     />
                 ))}
