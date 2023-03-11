6c4b033e26d3884c3811e3b5b35b1dce78f9d4d2
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-03-11 21:21:31 +0530

    Show button on my cart button using redux

--- 11-advanced-redux/src/components/Cart/CartButton.js
+++ 11-advanced-redux/src/components/Cart/CartButton.js
@@ -1,16 +1,17 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { uiAction } from '../../store/uiSlice';
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
   const dispatch = useDispatch();
+  const cartQuantity = useSelector((state)=>state.cart.totalQuantity)
   const toggleHandler = () => {
     dispatch(uiAction.toggle())
   }
   return (
     <button className={classes.button} onClick={toggleHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
