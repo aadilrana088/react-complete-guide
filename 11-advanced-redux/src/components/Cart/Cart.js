e5b57b82a4e5f10fd84c400e5ea315ecd0f40833
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-03-11 21:27:38 +0530

    Show item to cart section by fetching from redux

--- 11-advanced-redux/src/components/Cart/Cart.js
+++ 11-advanced-redux/src/components/Cart/Cart.js
@@ -1,18 +1,28 @@
+import { useSelector } from 'react-redux';
 import Card from '../UI/Card';
 import classes from './Cart.module.css';
 import CartItem from './CartItem';
-
 const Cart = (props) => {
-  return (
-    <Card className={classes.cart}>
-      <h2>Your Shopping Cart</h2>
-      <ul>
-        <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
-      </ul>
-    </Card>
-  );
+    const cartItem = useSelector((state) => state.cart.items);
+    console.log(cartItem);
+    return (
+        <Card className={classes.cart}>
+            <h2>Your Shopping Cart</h2>
+            <ul>
+                {cartItem.map((cart) => (
+                    <CartItem
+                        key={cart.id}
+                        item={{
+                            title: cart.name,
+                            quantity: cart.quantity,
+                            total: cart.totalPrice,
+                            price: cart.price,
+                        }}
+                    />
+                ))}
+            </ul>
+        </Card>
+    );
 };
 
 export default Cart;
