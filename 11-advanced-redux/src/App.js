9f7fb69312f15cad8e368d699c743a19bbe6a944
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-03-13 18:29:17 +0530

    Send request of PUT to firebase cart.json

--- 11-advanced-redux/src/App.js
+++ 11-advanced-redux/src/App.js
@@ -1,16 +1,25 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
-  const cartShow = useSelector(state => state.ui.cartIsVisible)
-  return (
-    <Layout>
-      {cartShow && <Cart /> }
-      <Products />
-    </Layout>
-  );
+    const cartShow = useSelector((state) => state.ui.cartIsVisible);
+    const cart = useSelector((state) => state.cart);
+    useEffect(() => {
+        fetch(
+            'https://react-http-60ae9-default-rtdb.firebaseio.com/cart.json',
+            { method: 'PUT', body: JSON.stringify(cart) }
+        );
+    }, [cart]);
+
+    return (
+        <Layout>
+            {cartShow && <Cart />}
+            <Products />
+        </Layout>
+    );
 }
 
 export default App;
