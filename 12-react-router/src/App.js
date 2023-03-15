9f29d2fe766c167ebd11f7c94644a859014fbf97
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-03-15 23:08:14 +0530

    Created router and show pages as per url

--- 12-react-router/src/App.js
+++ 12-react-router/src/App.js
@@ -1,5 +1,25 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Product from './pages/Product';
+import Welcome from './pages/Welcome';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/welcome",
+    element: <Welcome />
+  },
+  {
+    path: "/products",
+    element: <Product />
+  }
+])
+
 function App() {
-  return <div></div>;
+    return <RouterProvider router={router} />;
 }
 
 export default App;
