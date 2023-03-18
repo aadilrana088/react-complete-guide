14140de5573658c730ba9df000c63a5c9d31503d
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-03-18 12:15:52 +0530

    List of event with Link

--- 13-advanced-routing-frontend/src/pages/EventsPage.jsx
+++ 13-advanced-routing-frontend/src/pages/EventsPage.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
+const DUMMY_EVENTS = [
+    {
+        id: 'e1',
+        title: 'Some Events',
+    },
+    {
+        id: 'e2',
+        title: 'Another Events',
+    },
+];
 const EventsPage = () => {
-    return <div>EventsPage</div>;
+    return (
+        <div>
+            EventsPage
+            <ul>
+                {DUMMY_EVENTS.map((event) => (
+                    <li>
+                        <Link to={event.id} id={event.id}>
+                            {event.title}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default EventsPage;
