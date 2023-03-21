51073b8b81d875bc103a8c5793a3c77cdfac5ef3
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-03-22 01:44:52 +0530

    Show error message handling error

--- 13-advanced-routing-frontend/src/pages/Events.jsx
+++ 13-advanced-routing-frontend/src/pages/Events.jsx
@@ -3,7 +3,11 @@ import { useLoaderData } from 'react-router-dom';
 import EventsList from '../components/EventsList';
 
 const EventsPage = () => {
-    const events = useLoaderData();
+    const data = useLoaderData();
+    if(data.hasError) {
+        return <p>{data.message}</p>
+    }
+    const events = data.events
     return <EventsList events={events} />;
 };
 
@@ -12,9 +16,12 @@ export default EventsPage;
 export async function loader() {
     const response = await fetch('http://localhost:8080/events');
     if (!response.ok) {
-        // ...
+        return {
+            hasError: true,
+            message: "Could nor fetch events"
+        }
     } else {
         const resData = await response.json();
-        return resData.events;
+        return resData;
     }
 }
