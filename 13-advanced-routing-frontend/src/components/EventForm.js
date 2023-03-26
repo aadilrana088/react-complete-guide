e3cea2273f6c55a596225779d636919949cf23c1
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-03-26 11:22:16 +0530

    use server side validation

--- 13-advanced-routing-frontend/src/components/EventForm.js
+++ 13-advanced-routing-frontend/src/components/EventForm.js
@@ -1,8 +1,9 @@
-import { Form, useNavigate, useNavigation } from 'react-router-dom';
+import { Form, useActionData, useNavigate, useNavigation } from 'react-router-dom';
 
 import classes from './EventForm.module.css';
 
 function EventForm({ method, event }) {
+    const data = useActionData();
     const navigate = useNavigate();
     const navigation = useNavigation();
     const isSubmitting = navigation.state === 'submitting';
@@ -12,13 +13,18 @@ function EventForm({ method, event }) {
 
     return (
         <Form method="POST" className={classes.form}>
+            {data && data.errors && <ul>
+                {Object.values(data.errors).map((err) => (
+                    <li key={err}>{err}</li>
+                ))}
+                </ul>}
             <p>
                 <label htmlFor="title">Title</label>
                 <input
                     id="title"
                     type="text"
                     name="title"
-                    required
+                    // required
                     defaultValue={event ? event.title : ''}
                 />
             </p>
@@ -28,7 +34,7 @@ function EventForm({ method, event }) {
                     id="image"
                     type="url"
                     name="image"
-                    required
+                    // required
                     defaultValue={event ? event.image : ''}
                 />
             </p>
@@ -38,7 +44,7 @@ function EventForm({ method, event }) {
                     id="date"
                     type="date"
                     name="date"
-                    required
+                    // required
                     defaultValue={event ? event.date : ''}
                 />
             </p>
@@ -48,7 +54,7 @@ function EventForm({ method, event }) {
                     id="description"
                     name="description"
                     rows="5"
-                    required
+                    // required
                     defaultValue={event ? event.description : ''}
                 />
             </p>
