30329d3af625797d39de6abfc8de1bfa4858adf2
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-03-27 23:17:55 +0530

    use mode and useSearchParams for login form

--- 13-advanced-routing-frontend/src/components/AuthForm.js
+++ 13-advanced-routing-frontend/src/components/AuthForm.js
@@ -1,14 +1,10 @@
-import { useState } from 'react';
-import { Form } from 'react-router-dom';
+import { Form, Link, useSearchParams } from 'react-router-dom';
 
 import classes from './AuthForm.module.css';
 
 function AuthForm() {
-    const [isLogin, setIsLogin] = useState(true);
-
-    function switchAuthHandler() {
-        setIsLogin((isCurrentlyLogin) => !isCurrentlyLogin);
-    }
+    const [searchParams] = useSearchParams();
+    const isLogin = searchParams.get('mode') === "login"
 
     return (
         <>
@@ -28,9 +24,9 @@ function AuthForm() {
                     />
                 </p>
                 <div className={classes.actions}>
-                    <button onClick={switchAuthHandler} type="button">
+                    <Link to={`?mode=${isLogin ? "signup": "login"}`}>
                         {isLogin ? 'Create new user' : 'Login'}
-                    </button>
+                    </Link>
                     <button>Save</button>
                 </div>
             </Form>
