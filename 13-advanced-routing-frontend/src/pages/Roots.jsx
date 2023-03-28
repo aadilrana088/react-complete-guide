a7190b28a72d064ca32ad133d761af3f3446327e
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-03-28 23:57:38 +0530

    Expire token after 1 hour

--- 13-advanced-routing-frontend/src/pages/Roots.jsx
+++ 13-advanced-routing-frontend/src/pages/Roots.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
-import {Outlet} from "react-router-dom"
+import React, { useEffect } from 'react';
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 import MainNavigation from '../components/MainNavigation';
 
 const Roots = () => {
     // const navigation = useNavigation();
-    return <>
-        <MainNavigation />
-        {/* {navigation.state === "loading" && <p>Loading...</p>} */}
-        <main>
-            <Outlet />
-        </main>
-    </>;
+    const token = useLoaderData();
+    const submit = useSubmit();
+
+    useEffect(() => {
+        if (!token) {
+            return;
+        }
+
+        setTimeout(() => {
+            submit(null, { action: '/logout', method: 'POST' });
+        }, 1 * 60 * 60 * 1000);
+    }, [token, submit]);
+
+    return (
+        <>
+            <MainNavigation />
+            {/* {navigation.state === "loading" && <p>Loading...</p>} */}
+            <main>
+                <Outlet />
+            </main>
+        </>
+    );
 };
 
 export default Roots;
