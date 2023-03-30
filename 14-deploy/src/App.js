84e03a5ff07f92a6f4b0e057c33fbfcea957aa24
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-03-30 22:58:31 +0530

    added lazy loading for Blog and Post

--- 14-deploy/src/App.js
+++ 14-deploy/src/App.js
@@ -1,32 +1,58 @@
+import { lazy, Suspense } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import BlogPage, { loader as postsLoader } from './pages/Blog';
+// import BlogPage, { loader as postsLoader } from './pages/Blog';
 import HomePage from './pages/Home';
-import PostPage, { loader as postLoader } from './pages/Post';
+// import PostPage, { loader as postLoader } from './pages/Post';
 import RootLayout from './pages/Root';
 
+const BlogPage = lazy(() => import('./pages/Blog'));
+const PostPage = lazy(() => import('./pages/Post'));
+
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <RootLayout />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: 'posts',
+    {
+        path: '/',
+        element: <RootLayout />,
         children: [
-          { index: true, element: <BlogPage />, loader: postsLoader },
-          { path: ':id', element: <PostPage />, loader: postLoader },
+            {
+                index: true,
+                element: <HomePage />,
+            },
+            {
+                path: 'posts',
+                children: [
+                    {
+                        index: true,
+                        element: (
+                            <Suspense fallback={<p>Loading....</p>}>
+                                <BlogPage />
+                            </Suspense>
+                        ),
+                        loader: () =>
+                            import('./pages/Blog').then((module) =>
+                                module.loader()
+                            ),
+                    },
+                    {
+                        path: ':id',
+                        element: (
+                            <Suspense fallback={<p>Loading...</p>}>
+                                <PostPage />
+                            </Suspense>
+                        ),
+                        loader: (meta) =>
+                            import('./pages/Post').then((module) =>
+                                module.loader(meta)
+                            ),
+                    },
+                ],
+            },
         ],
-      },
-    ],
-  },
+    },
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+    return <RouterProvider router={router} />;
 }
 
 export default App;
