e5ea787a4342811154758612b3cbb5bdf04bc127
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-03-31 08:29:33 +0530

    React basic part covered

--- 15-nextjs-course/pages/news/index.js
+++ 15-nextjs-course/pages/news/index.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import Link from 'next/link';
+import { Fragment } from 'react';
 
-const NewsPage = () => {
-    return <div>NewsPage</div>;
-};
+function NewsPage() {
+    return (
+        <Fragment>
+            <h1>The News Page</h1>
+            <ul>
+                <li>
+                    <Link href="/news/nextjs-is-a-great-framework">
+                        NextJS Is A Great Framework
+                    </Link>
+                </li>
+                <li>Something Else</li>
+            </ul>
+        </Fragment>
+    );
+}
 
 export default NewsPage;
+    
