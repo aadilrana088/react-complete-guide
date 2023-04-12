f7210657e9c4850fd2eee57f3bc06be9e3d7a89c
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-04-12 16:29:35 +0530

    Fect data from db and show the list

--- 16-nextjs-meetup/pages/index.js
+++ 16-nextjs-meetup/pages/index.js
@@ -1,26 +1,9 @@
+import { MongoClient } from 'mongodb';
+
 import MeetupList from '../components/meetups/MeetupList';
 
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'A First Meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Some address 5, 12345 Some City',
-    description: 'This is a first meetup!',
-  },
-  {
-    id: 'm2',
-    title: 'A Second Meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Some address 10, 12345 Some City',
-    description: 'This is a second meetup!',
-  },
-];
-
 function HomePage(props) {
-  return <MeetupList meetups={props.meetups} />;
+    return <MeetupList meetups={props.meetups} />;
 }
 
 // export async function getServerSideProps(context) {
@@ -37,13 +20,27 @@ function HomePage(props) {
 // }
 
 export async function getStaticProps() {
-  // fetch data from an API
-  return {
-    props: {
-      meetups: DUMMY_MEETUPS
-    },
-    revalidate: 1
-  }; 
+    // fetch data from an API
+    const client = await MongoClient.connect(process.env.MONGO_URI);
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const meetups = await meetupsCollection.find().toArray();
+
+    client.close();
+
+    return {
+        props: {
+            meetups: meetups.map((meetup) => ({
+                title: meetup.title,
+                address: meetup.address,
+                image: meetup.image,
+                id: meetup._id.toString(),
+            })),
+        },
+        revalidate: 1,
+    };
 }
 
 export default HomePage;
