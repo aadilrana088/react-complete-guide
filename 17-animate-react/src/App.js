697ba1c634a1fa8baefb0b4dc38407d6e6bb4888
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-04-12 23:50:24 +0530

    Show modal using conditional operator

--- 17-animate-react/src/App.js
+++ 17-animate-react/src/App.js
@@ -19,8 +19,14 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
-        <Backdrop show={this.state.modalIsOpen} />
+        {this.state.modalIsOpen ? (
+          <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+        ) : null}
+        {/* <Modal show={this.state.modalIsOpen} closed={this.closeModal} /> */}
+        {this.state.modalIsOpen ? (
+          <Backdrop show={this.state.modalIsOpen} />
+        ) : null}
+        {/* <Backdrop show={this.state.modalIsOpen} /> */}
         <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
