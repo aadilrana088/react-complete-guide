74be9f83375beab10acb96042dd89043fb90f5af
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-04-18 17:55:14 +0530

    Memonized the changes for for custom store

--- 18-react-hook/src/components/Products/ProductItem.js
+++ 18-react-hook/src/components/Products/ProductItem.js
@@ -5,30 +5,30 @@ import Card from '../UI/Card';
 import { useStore } from '../../hooks-store/store';
 import './ProductItem.css';
 
-const ProductItem = props => {
+const ProductItem = React.memo((props) => {
+    // const toggleFav = useContext(ProductsContext).toggleFav;
+    console.log('RENDERING');
+    const dispatch = useStore(false)[1];
 
-  // const toggleFav = useContext(ProductsContext).toggleFav;
-  const dispatch = useStore()[1];
+    const toggleFavHandler = () => {
+        // toggleFav(props.id);
+        dispatch('TOGGLE_FAV', props.id);
+    };
 
-  const toggleFavHandler = () => {
-    // toggleFav(props.id);
-    dispatch('TOGGLE_FAV', props.id);
-  };
-
-  return (
-    <Card style={{ marginBottom: '1rem' }}>
-      <div className="product-item">
-        <h2 className={props.isFav ? 'is-fav' : ''}>{props.title}</h2>
-        <p>{props.description}</p>
-        <button
-          className={!props.isFav ? 'button-outline' : ''}
-          onClick={toggleFavHandler}
-        >
-          {props.isFav ? 'Un-Favorite' : 'Favorite'}
-        </button>
-      </div>
-    </Card>
-  );
-};
+    return (
+        <Card style={{ marginBottom: '1rem' }}>
+            <div className="product-item">
+                <h2 className={props.isFav ? 'is-fav' : ''}>{props.title}</h2>
+                <p>{props.description}</p>
+                <button
+                    className={!props.isFav ? 'button-outline' : ''}
+                    onClick={toggleFavHandler}
+                >
+                    {props.isFav ? 'Un-Favorite' : 'Favorite'}
+                </button>
+            </div>
+        </Card>
+    );
+});
 
 export default ProductItem;
