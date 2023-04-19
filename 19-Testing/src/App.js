2305b91481442634633a113498597af75f9bf50a
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-04-19 17:27:35 +0530

    Async request test using mock function

--- 19-Testing/src/App.js
+++ 19-Testing/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Async from './components/Async';
 import Greeting from './components/Greeting';
 
 function App() {
   return (
     <div className="App">
       <Greeting />
+      <Async />
     </div>
   );
 }
