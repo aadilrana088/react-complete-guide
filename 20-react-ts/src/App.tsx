edb981fb6772ee7f0f3fa407feab6414bb6214ae
Mohd Aadil Rana <aadilrana088@gmail.com>
2023-04-20 18:36:18 +0530

    Clean code write ts omponent and added extra typescript code

---
+++ 20-react-ts/src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+import Todos from "./components/Todos"
+
+function App() {
+  return (
+    <Todos items={['Learn React', 'Learn TypeScript']} />
+  );
+}
+
+export default App;
