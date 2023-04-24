c33584a39d713a3dbc9edc18035eec8977935d89
Mohd Aadil Rana <epochbrokers2gmail.com>
2023-04-24 14:41:43 +0530

    react-http firebase data changed

--- 07-http-request/src/App.js
+++ 07-http-request/src/App.js
@@ -10,7 +10,7 @@ function App() {
         setIsLoading(true);
         setError(null);
         try {
-            const response = await fetch('https://react-http-60ae9-default-rtdb.firebaseio.com/movies.json');
+            const response = await fetch('https://react-http-2a09f-default-rtdb.firebaseio.com/movies.json');
             if (!response.ok) {
                 throw new Error('Something went wrong!');
             }
@@ -45,7 +45,7 @@ function App() {
     }, [fetchMoviesHandler]);
 
     async function addMovieHandler(movie) {
-        const response = await fetch('https://react-http-60ae9-default-rtdb.firebaseio.com/movies.json',{
+        const response = await fetch('https://react-http-2a09f-default-rtdb.firebaseio.com/movies.json',{
             method: 'POST',
             body: JSON.stringify(movie),
             headers: {
--- 11-advanced-redux/src/store/cartAction.js
+++ 11-advanced-redux/src/store/cartAction.js
@@ -4,7 +4,7 @@ export const fetchCartData = () => {
     return async (dispatch) => {
         const fetchData = async () => {
             const response = await fetch(
-                'https://react-http-60ae9-default-rtdb.firebaseio.com/cart.json'
+                'https://react-http-2a09f-default-rtdb.firebaseio.com/cart.json'
             );
             if (!response.ok) {
                 throw new Error('Could not fetch Data');
@@ -43,7 +43,7 @@ export const sendCartData = (cart) => {
 
         const sendRequest = async () => {
             const response = await fetch(
-                'https://react-http-60ae9-default-rtdb.firebaseio.com/cart.json',
+                'https://react-http-2a09f-default-rtdb.firebaseio.com/cart.json',
                 {
                     method: 'PUT',
                     body: JSON.stringify(cart),
